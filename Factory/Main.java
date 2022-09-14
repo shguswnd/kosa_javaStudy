981e82a095eca99b0e2a62ac76d7b1c948825667
shguswnd <nhj6336@gmail.com>
2022-09-15 00:18:56 +0900

    FactoryPattern1

---
+++ Factory/Main.java
@@ -0,0 +1,13 @@
+package Factory;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Computer pc = ComputerFactory.getComputer("pc", "2 GB", "500 GB", "2.4 GHz");
+		Computer server = ComputerFactory.getComputer("server", "15 GB", "5 GB", "5.5 GHz");
+		System.out.println("Factory PC Config::"+pc);
+		System.out.println("Factory Server Config::"+server);
+	}
+
+}
