06a686f5ab30adbea4eef2ee117262666320ae39
shguswnd <nhj6336@gmail.com>
2022-09-16 00:26:52 +0900

    Template

---
+++ Template/Tea.java
@@ -0,0 +1,10 @@
+package Template;
+
+public class Tea extends CaffeineBeverage{
+	public void brew() {
+		System.out.println("찻잎을 우려내는 중");
+	}
+	public void addCondiments() {
+		System.out.println("레몬을 추가하는 중");
+	}
+}
